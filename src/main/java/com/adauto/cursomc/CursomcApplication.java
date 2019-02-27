7189eb9652a5ea06b12086a26712e38bb8b9647e
Adauto Mourato <ajmbf@cin.ufpe.br>
2019-02-27 09:47:01 -0300

    Criando operação de instanciaçao para add os objetos no banco

--- src/main/java/com/adauto/cursomc/CursomcApplication.java
+++ src/main/java/com/adauto/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.adauto.cursomc;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.adauto.cursomc.domain.Categoria;
+import com.adauto.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriarepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception { //instanciando objetos para salvar no bd
+		
+		Categoria cat1 = new Categoria(null,"informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriarepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
